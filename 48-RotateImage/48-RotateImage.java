609b8abee246d06788edffebdec515a5719140c5
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-08-01 11:57:36 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.java
@@ -0,0 +1,57 @@
+// Last updated: 8/1/2025, 11:57:36 AM
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    // Utility method to print the matrix (optional for testing purposes)
+    public void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        solution.rotate(matrix1);
+        solution.printMatrix(matrix1); // Expected output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+        int[][] matrix2 = {
+            {5, 1, 9, 11},
+            {2, 4, 8, 10},
+            {13, 3, 6, 7},
+            {15, 14, 12, 16}
+        };
+        
+        solution.rotate(matrix2);
+        solution.printMatrix(matrix2); // Expected output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+    }
+}
