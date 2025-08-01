98762e3359f99093222e04cb91884f050bc9aa5d
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-08-01 11:57:46 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.java
@@ -0,0 +1,27 @@
+// Last updated: 8/1/2025, 11:57:45 AM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
+        Combination(candidates, target, ll, 0, ans);
+        return ans;
+    }
+
+    public static void Combination(int[] coin, int amount, List<Integer> ll,int idx, List<List<Integer>> ans) {
+        if (amount == 0) {
+            ans.add(new ArrayList<>(ll)); 
+            return;
+        }
+
+        for (int i = idx; i < coin.length; i++) {
+            if (amount >= coin[i]) {
+                ll.add(coin[i]); 
+                Combination(coin, amount - coin[i], ll, i, ans); 
+                ll.remove(ll.size() - 1); 
+            }
+        }
+    }
+}
