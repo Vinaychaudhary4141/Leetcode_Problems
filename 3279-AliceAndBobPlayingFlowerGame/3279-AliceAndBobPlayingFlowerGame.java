2c32a810f82464f9b605624d66c3824539fc2526
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-09-21 19:55:27 +0530

    Create 3279-AliceAndBobPlayingFlowerGame

---
+++ 3279-AliceAndBobPlayingFlowerGame/3279-AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,9 @@
+// Last updated: 9/21/2025, 7:55:21 PM
+class Solution {
+    public long flowerGame(int n, int m) {
+        long x=m;
+        long y=n;
+        
+      return (x*y)/2;  
+    }
+}
