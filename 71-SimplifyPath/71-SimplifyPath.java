3f86e4eb403212c0b535f78e3f1a642ac0fe97c3
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-09-21 19:57:42 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -0,0 +1,21 @@
+// Last updated: 9/21/2025, 7:57:36 PM
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr=path.split("/");
+        Stack<String> st= new Stack<>();
+
+        for(String i:arr){
+           if(i.equals("..")&& !st.empty()){
+               st.pop();
+           }else if(!i.equals("")&& !i.equals(".") && !i.equals("..")){
+            st.push(i);
+           }
+        }
+        StringBuilder sb= new StringBuilder();
+        for(String i: st){
+            sb.append("/");
+            sb.append(i);
+        }
+        return sb.length() ==0 ? "/" : sb.toString();
+    }
+}
