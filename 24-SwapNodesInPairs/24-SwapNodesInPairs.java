0ccf76be712d792427b852ad6cbd9e51ed0670f5
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-09-21 19:58:23 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.java
@@ -0,0 +1,25 @@
+// Last updated: 9/21/2025, 7:58:17 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode first=head;
+        ListNode second=head.next;
+        first.next=swapPairs(second.next);
+         second.next = first;
+
+        return second;
+        
+    }
+}
