f8a2e9c370a1e44e280d7b32cd76cec832122501
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-11-12 11:35:27 +0530

    Create 3620-MaximumNumberOfDistinctElementsAfterOperations

---
+++ 3620-MaximumNumberOfDistinctElementsAfterOperations/3620-MaximumNumberOfDistinctElementsAfterOperations.java
@@ -0,0 +1,16 @@
+// Last updated: 11/12/2025, 11:35:18 AM
+class Solution {
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0, prev = (int)-1e9;
+
+        for (int x : nums) {
+            int l = Math.max(x - k, prev + 1);
+            if (l <= x + k) {
+                prev = l;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
