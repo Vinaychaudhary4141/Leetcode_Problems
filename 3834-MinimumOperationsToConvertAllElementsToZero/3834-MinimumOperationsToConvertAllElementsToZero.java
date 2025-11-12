25fe1140ce843fb893fbf8c56036fcf809a268d9
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-11-12 11:35:15 +0530

    Create 3834-MinimumOperationsToConvertAllElementsToZero

---
+++ 3834-MinimumOperationsToConvertAllElementsToZero/3834-MinimumOperationsToConvertAllElementsToZero.java
@@ -0,0 +1,19 @@
+// Last updated: 11/12/2025, 11:35:07 AM
+class Solution {
+
+    public int minOperations(int[] nums) {
+        List<Integer> s = new ArrayList<>();
+        int res = 0;
+        for (int a : nums) {
+            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
+                s.remove(s.size() - 1);
+            }
+            if (a == 0) continue;
+            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+                res++;
+                s.add(a);
+            }
+        }
+        return res;
+    }
+}
