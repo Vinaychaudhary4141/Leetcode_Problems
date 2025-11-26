c3c554cb5431ad7cb4be4e707e558fe53e5f96ae
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-11-26 12:59:55 +0530

    Create 1388-GreatestSumDivisibleByThree

---
+++ 1388-GreatestSumDivisibleByThree/1388-GreatestSumDivisibleByThree.java
@@ -0,0 +1,19 @@
+// Last updated: 11/26/2025, 12:59:47 PM
+class Solution {
+
+        public int maxSumDivThree(int[] nums) {
+                int[] f = { 0, Integer.MIN_VALUE, Integer.MIN_VALUE };
+                        for (int num : nums) {
+                                    int[] g = new int[3];
+                                                System.arraycopy(f, 0, g, 0, 3);
+                                                            for (int i = 0; i < 3; ++i) {
+                                                                            g[(i + (num % 3)) % 3] = Math.max(
+                                                                                                g[(i + (num % 3)) % 3],
+                                                                                                                    f[i] + num
+                                                                                                                                    );
+                                                                                                                                                }
+                                                                                                                                                            f = g;
+                                                                                                                                                                    }
+                                                                                                                                                                            return f[0];
+                                                                                                                                                                                }
+                                                                                                                                                                                }
