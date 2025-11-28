7a9a60c497ec36e3d31d1156ac310d8f311df437
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2025-11-28 21:47:53 +0530

    Create 526-BeautifulArrangement

---
+++ 526-BeautifulArrangement/526-BeautifulArrangement.java
@@ -0,0 +1,22 @@
+// Last updated: 11/28/2025, 9:47:44 PM
+class Solution {
+    int count = 0;
+    public int countArrangement(int n) {
+        boolean[] used = new boolean[n + 1];
+        backtrack(n, 1, used);
+        return count;
+    }
+    private void backtrack(int n, int pos, boolean[] used) {
+        if (pos > n) {
+            count++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!used[i] && (i % pos == 0 || pos % i == 0)) {
+                used[i] = true;
+                backtrack(n, pos + 1, used);
+                used[i] = false;
+            }
+        }
+    }
+}
