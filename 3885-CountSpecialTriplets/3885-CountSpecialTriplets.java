5d3dac411e9d7da66d5aa7e588d9cf3b7669889d
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2026-01-16 22:12:48 +0530

    Create 3885-CountSpecialTriplets

---
+++ 3885-CountSpecialTriplets/3885-CountSpecialTriplets.java
@@ -0,0 +1,26 @@
+// Last updated: 1/16/2026, 10:12:38 PM
+class Solution {
+
+    public int specialTriplets(int[] nums) {
+        final int MOD = 1000000007;
+        Map<Integer, Integer> numCnt = new HashMap<>();
+        Map<Integer, Integer> numPartialCnt = new HashMap<>();
+
+        for (int v : nums) {
+            numCnt.put(v, numCnt.getOrDefault(v, 0) + 1);
+        }
+
+        long ans = 0;
+        for (int v : nums) {
+            int target = v * 2;
+            int lCnt = numPartialCnt.getOrDefault(target, 0);
+            numPartialCnt.put(v, numPartialCnt.getOrDefault(v, 0) + 1);
+            int rCnt =
+                numCnt.getOrDefault(target, 0) -
+                numPartialCnt.getOrDefault(target, 0);
+            ans = (ans + (long) lCnt * rCnt) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
