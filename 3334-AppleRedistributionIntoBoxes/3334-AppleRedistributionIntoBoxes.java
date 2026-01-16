f644baddab46d1acfe6f9e1d771ab7db9bbda1c2
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2026-01-16 22:13:31 +0530

    Create 3334-AppleRedistributionIntoBoxes

---
+++ 3334-AppleRedistributionIntoBoxes/3334-AppleRedistributionIntoBoxes.java
@@ -0,0 +1,25 @@
+// Last updated: 1/16/2026, 10:13:21 PM
+class Solution {
+
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum = 0;
+        for (int a : apple) {
+            sum += a;
+        }
+
+        Integer[] capArray = new Integer[capacity.length];
+        for (int i = 0; i < capacity.length; i++) {
+            capArray[i] = capacity[i];
+        }
+
+        Arrays.sort(capArray, Collections.reverseOrder());
+
+        int need = 0;
+        while (sum > 0) {
+            sum -= capArray[need];
+            need += 1;
+        }
+
+        return need;
+    }
+}
