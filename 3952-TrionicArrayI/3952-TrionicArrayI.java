66436ed848a732499f903954d6467eb45be96ffe
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2026-02-11 12:15:11 +0530

    Create 3952-TrionicArrayI

---
+++ 3952-TrionicArrayI/3952-TrionicArrayI.java
@@ -0,0 +1,24 @@
+// Last updated: 2/11/2026, 12:15:09 PM
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        if (i == 0) return false; 
+
+        int p = i;
+        while (i + 1 < n && nums[i] > nums[i + 1]) {
+            i++;
+        }
+        if (i == p) return false;
+
+        int q = i;
+        while (i + 1 < n && nums[i] < nums[i + 1]) {
+            i++;
+        }
+        if (i == q) return false; 
+        return i == n - 1;
+    }
+}
