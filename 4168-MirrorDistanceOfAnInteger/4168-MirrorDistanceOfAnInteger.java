9bfd27843f2a9d180fc9310652d8d427ada69e99
Vinaychaudhary4141 <142567617+Vinaychaudhary4141@users.noreply.github.com>
2026-07-19 18:53:33 +0530

    Create 4168-MirrorDistanceOfAnInteger

---
+++ 4168-MirrorDistanceOfAnInteger/4168-MirrorDistanceOfAnInteger.java
@@ -0,0 +1,16 @@
+// Last updated: 7/19/2026, 6:53:34 PM
+class Solution {
+
+    public int reverse(int n) {
+        int res = 0;
+        while (n > 0) {
+            res = res * 10 + (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
+
+    public int mirrorDistance(int n) {
+        return Math.abs(n - reverse(n));
+    }
+}
